8d319c24e80a82746445a26041d03bba709b50ad
abdalwahab <bayanadam10@gmail.com>
2020-05-06 21:23:01 +0300

    Initial commit

---
+++ app/src/main/java/com/abdalwahab/design_section/PostList.kt
@@ -0,0 +1,12 @@
+package com.abdalwahab.design_section
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_list)
+    }
+}
---
+++ app/src/main/java/com/abdalwahab/design_section/PostsActivity.kt
@@ -0,0 +1,12 @@
+package com.abdalwahab.design_section
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posts)
+    }
+}
